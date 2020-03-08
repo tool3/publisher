7c6528078928d10e4582bc1c4eeea395a3f96f25
Tal Hayut <talhayut.dev@gmail.com>
2020-03-08 19:07:03 +0200

    hacking about

--- index.js
+++ index.js
@@ -4,35 +4,35 @@ const { Toolkit } = require('actions-toolkit');
 
 Toolkit.run(async tools => {
   {
-      const scope = core.getInput('scope');
+    const scope = core.getInput('scope');
 
-      const registries = {
-        github: {
-          url: 'npm.pkg.github.com',
-          token: core.getInput('github_token')
-        },
-        npm: {
-          url: 'registry.npmjs.org',
-          token: core.getInput('npm_token')
-        }
-      };
+    const registries = {
+      github: {
+        url: 'npm.pkg.github.com',
+        token: core.getInput('github_token')
+      },
+      npm: {
+        url: 'registry.npmjs.org',
+        token: core.getInput('npm_token')
+      }
+    };
 
-      tools.log(`using scope: ${scope}`);
+    tools.log(`using scope: ${scope}`);
 
-      Object.keys(registries).forEach(async registry => {
-        const { url, token } = registries[registry];
-        tools.log(`Publishing to ${registry}...`);
-        try {
-          core.group(registry);
-          exec('echo', [`//${url}/:_authToken=${token}`, '>', `.npmrc`]).then(() => {
-            exec('npm', ['publish']);
-          });
-        } catch (error) {
-          core.setFailed(`Failed to publish! ${error.message}`);
-        }
-        
-        tools.log(`Successfully published to ${registry} !`);
-        core.ExitCode(0);
+    Object.keys(registries).forEach(registry => {
+      core.group(registry);
+      const { url, token } = registries[registry];
+      tools.log(`Publishing to ${registry}...`);
+
+      exec('echo', [`//${url}/:_authToken=${token}`, '>', `.npmrc`]).then(() => {
+        exec('npm', ['publish']);
+      }).catch(error => {
+        core.setFailed(`Failed to publish! ${error.message}`);
       });
-    }
+
+      tools.log(`Successfully published to ${registry} !`);
+    });
+
+    core.ExitCode(0);
+  }
 });
