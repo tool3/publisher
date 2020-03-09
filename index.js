977b7c3cfdd524b14ce8c2777aa3371e7ff136da
Tal Hayut <talhayut.dev@gmail.com>
2020-03-09 09:23:46 +0200

    using await reduce

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 const core = require('@actions/core');
 const { exec } = require('@actions/exec');
+const { Toolkit } = require('actions-toolkit');
 
-async function run() {
-  try {
-    const scope = core.getInput('scope');
+Toolkit.run(async tools => {
+  {
+    try {
+      const scope = core.getInput('scope');
 
-    const registries = {
-      github: {
-        url: 'npm.pkg.github.com',
-        token: core.getInput('github_token')
-      },
-      npm: {
-        url: 'registry.npmjs.org',
-        token: core.getInput('npm_token')
-      }
-    };
+      const registries = {
+        github: {
+          url: 'npm.pkg.github.com',
+          token: core.getInput('github_token')
+        },
+        npm: {
+          url: 'registry.npmjs.org',
+          token: core.getInput('npm_token')
+        }
+      };
 
-    core.info(`using scope: ${scope}`);
+      core.info(`using scope: ${scope}`);
 
-    Object.keys(registries).forEach(async registry => {
-      core.group(registry);
-      const { url, token } = registries[registry];
-      core.info('', `Publishing to ${registry}...`);
-      await exec('npm', ['publish', `--registry=https://${url}/:_authToken=${token}`]);
-      core.info(`Successfully published to ${registry} !`);
-    });
+      await Object.keys(registries).reduce(async (promise, registry) => {
+        await promise;
+        
+        core.group(registry);
+        const { url, token } = registries[registry];
+        tools.log(`Publishing to ${registry}...`);
+        await exec('npm', ['publish', `--registry=https://${url}/:_authToken=${token}`]);
+        tools.log(`Successfully published to ${registry} !`);
 
-  } catch (error) {
-    core.setFailed(`Failed to publish ${error.message}`);
+      }, Promise.resolve());
+      
+    } catch (error) {
+      core.error(error);
+      core.setFailed(`Failed to publish ${error.message}`);
+    }
   }
-}
+});
 
-run();
+
